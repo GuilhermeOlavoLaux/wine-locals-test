4272193b95888c3600988a698c702d56ac7f5be7
GuilhermeOlavoLaux <guilherme.laux@compliancetotal.com.br>
2022-05-23 20:11:10 -0300

    create the project template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Restaurants from "./components/Restaurants";
+import './index.css'
+
+function App() {
+  return (
+    <Fragment>
+      <Restaurants />
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ src/components/Restaurants.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+export default function Restaurants() {
+
+
+
+
+
+    return (
+        <Fragment>
+            <h1>KKKK</h1>
+
+
+        </Fragment>
+    )
+}
