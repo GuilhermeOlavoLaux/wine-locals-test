e73c5a51f0fde9d76144a819c2107f4ad46fe3a9
GuilhermeOlavoLaux <guilherme.laux@compliancetotal.com.br>
2022-05-25 23:14:22 -0300

    styles adjusts

--- src/components/NewDish.jsx
+++ src/components/NewDish.jsx
@@ -30,43 +30,47 @@ export default function NewDish() {
                         <Header backButtonFlag={true}></Header>
                         <div className="new-dish-content">
                             <h1>{restaurant.name}</h1>
-                            <p>Nome do prato</p>
-                            <input
-                                type="text"
-                                className="name-input"
-                                value={name}
-                                placeholder="Prato"
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                            <div className="price-input-container">
-                                <p>Valor</p>
 
-                                <label htmlFor="price-input" className="label-price-input" >
-                                    <p>R$</p>
-                                    <input
-                                        type="number"
-                                        id='price-input'
-                                        value={price}
-                                        className="price-input"
-                                        placeholder="0,00"
-                                        onChange={(e) => setPrice(e.target.value)}
-                                    />
-                                </label>
+                            <div className="new-dish-form">
+
+                                <p>Nome do prato</p>
+                                <input
+                                    type="text"
+                                    className="name-input"
+                                    value={name}
+                                    placeholder="Prato"
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                <div className="price-input-container">
+                                    <p>Valor</p>
+
+                                    <label htmlFor="price-input" className="label-price-input" >
+                                        <p>R$</p>
+                                        <input
+                                            type="number"
+                                            id='price-input'
+                                            value={price}
+                                            className="price-input"
+                                            placeholder="0,00"
+                                            onChange={(e) => setPrice(e.target.value)}
+                                        />
+                                    </label>
+
+                                </div>
+                                <p>Descrição do prato</p>
+
+                                <textarea cols="30"
+                                    rows="10"
+                                    value={description}
+                                    maxLength='200'
+                                    placeholder="Insira uma descrição"
+                                    onChange={(e) => setDescription(e.target.value)}>
+                                </textarea>
+
+                                <p className="description-text">*A descrição deve conter até 200 caracteres.</p>
+                                <button className="save-button">Salvar</button>
 
                             </div>
-                            <p>Descrição do prato</p>
-
-                            <textarea cols="30"
-                                rows="10"
-                                value={description}
-                                maxLength='200'
-                                placeholder="Insira uma descrição"
-                                onChange={(e) => setDescription(e.target.value)}>
-                            </textarea>
-
-                            <p className="description-text">*A descrição deve conter até 200 caracteres.</p>
-                            <button className="save-button">Salvar</button>
-
                         </div>
                     </div>
                 </div>
